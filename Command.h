37f69fe1b36f19a00ba282c5b80e9375530e2963
Justin Schopick <jschopick15@gmail.com>
2017-11-02 02:53:24 -0700

    Changed all strings to char* within class

--- Command.h
+++ Command.h
@@ -3,16 +3,14 @@
 
 #include "Base.h"
 #include <vector>
-#include <cstring>
-#include <stdio.h>
 
 class Command : public Base {
  protected:
     std::vector<char*> commands;
  public:
     Command();
-    Command(std::vector<std::string>);
-    void setComVector(std::string);
+    Command(std::vector<char*>);
+    void setComVector(char*);
     virtual bool execute();
 };
 
