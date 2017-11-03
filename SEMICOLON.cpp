c098ce17d8229b10818fc907daa8a049e0cd604e
Justin Schopick <jschopick15@gmail.com>
2017-11-03 12:19:17 -0700

    Initialized SEMICOLON class

---
+++ SEMICOLON.cpp
@@ -0,0 +1,14 @@
+#include "SEMICOLON.h"
+
+// class SEMICOLON : public Connector {
+//  public:
+
+// Pushes back a ";" character into the connector vector.
+void SEMICOLON::setConVector(char* Semicolon) {
+    connectors.push_back(Semicolon);
+}
+
+// Verifies that the && has been executed.
+bool SEMICOLON::execute() {
+
+}
