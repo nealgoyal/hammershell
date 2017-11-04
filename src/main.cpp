32df9b6d84488b3b91ad6b7e75f96bd7196dc47a
Justin Schopick <jschopick15@gmail.com>
2017-11-03 17:30:16 -0700

    changed main to reflect github

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include "Base.h"
 #include "Command.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2)
+    {
+      std::cout << "Error: need more arguments." << std::endl;
+      return 1;
+    }
+
+    std::string input = "";
+    for(int i = 1; i < argc; ++i) {
+      input.append(argv[i]);
+    }
+    std::cout << input << std::endl;
+    // Command* cmd = new Command();
+    // cmd->setComVector(input);
+    // cmd->display();
     return 0;
 }
