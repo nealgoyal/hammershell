6f06a0f8c4d9a9aa1b023704302220fd9b14202a
Neal Goyal <ngoya001@ucr.edu>
2017-11-04 15:09:33 -0700

    Modified Exit.cpp

--- src/Exit.cpp
+++ src/Exit.cpp
@@ -4,9 +4,10 @@
 
 // Checks if there are any commands in the vector. If not, exit and return true.
 bool Exit::execute(std::vector<char*>) {
-    if(commands.empty()) {
-        exit(0);
-        return true;
-    }
-    return false;
+    // if(commands.empty()) {
+    //     exit(0);
+    //     return true;
+    // }
+    // return false;
+    exit(0);
 }
