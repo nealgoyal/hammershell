fadf7204ebc2816c99b0242f511442edca686990
Neal Goyal <ngoya001@ucr.edu>
2017-11-19 20:24:40 -0800

    Created precedence functionality for one ()

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "SEMICOLON.h"
 
 #include <boost/algorithm/string.hpp>
+#include <vector>
 using namespace std;
 using namespace boost;
 
@@ -18,20 +19,23 @@ using namespace boost;
 // Base* createTree(Base*, Base*, Base*, std::string);
 
 int main() {
-    while(true) { // Prompts user again until they type exit.
-        std::string oldstr = "";
-        std::cout << "$ ";
-        std::getline(std::cin, oldstr);
+    while(true) {
 
-        std::string str = oldstr.substr(0, oldstr.find("#", 0));
+      // Prompts user again until they type exit.
+        std::string oldstr1 = "";
+        std::cout << "$ ";
+        std::getline(std::cin, oldstr1);
+
+        std::string oldstr2 = oldstr1.substr(0, oldstr1.find("#", 0));
 
         int count = 0;
+        //--------------------------------------------------------------------------------------------
         // checks if balanced parentheses. If not, throw error
-        for(unsigned i = 0; i < str.length(); ++i) {
-            if(str.at(i) == '(') {
+        for(unsigned i = 0; i < oldstr2.length(); ++i) {
+            if(oldstr2.at(i) == '(') {
                 ++count;
             }
-            if(str.at(i) == ')') {
+            if(oldstr2.at(i) == ')') {
                 --count;
                 if(count < 0) {
                     // perror("bash: syntax error near unexpected token ')'");
@@ -45,49 +49,113 @@ int main() {
             exit(1);
         }
 
-        // Attempt 1: insert contents of parenthesis in front of entire string
-        // Note: This will not work properly. Must find a way to parse string inside parentheses
-        // Possible solution: Base* buildTree(Base* left, Base* right, &string)
-        // Pass in temp string. Return Base* root node and update temp string inside
 
-        // Checks if we need to deal with precedence
-        // if(str.find('(') != std::string::npos) {
-        //     std::size_t leftParenth = str.find('(');
-        //     std::size_t rightParenth = str.rfind(')');
-        //     std::string temp = str.substr(leftParenth + 1, rightParenth - (leftParenth + 1));
-        //     cout << "Original string: " << str << endl;
-        //     cout << "String in Parentheses: " << temp << endl;
-        //     str.erase(leftParenth, (rightParenth + 1) - leftParenth );
-        //     cout << "Original string after removal of parentheses: " << str << endl << endl;
-        //     // str.insert(0, temp + " ");
-        //     // cout << "String after reinsertion: " << str << endl;
-        // }
-
-        if(str.find('(') == std::string::npos) {
-            //----------------------------------------------
-            //parsing for any [] -> test
-            vector<char> tester(str.begin(), str.end());
-            for(unsigned i = 0; i < tester.size(); ++i) {
-                if(tester.at(i) == '[') {
-                    tester.erase(tester.begin() + i);
-                    for(unsigned j = i + 1; j < tester.size(); ++j) {
-                        if(tester.at(j) == ']') {
-                            tester.erase(tester.begin() + j);
-                        }
+        //--------------------------------------------------------------------------------------------
+        //parsing for any [] -> test
+        vector<char> tester(oldstr2.begin(), oldstr2.end());
+        for(unsigned i = 0; i < tester.size(); ++i) {
+            if(tester.at(i) == '[') {
+                tester.erase(tester.begin() + i);
+                for(unsigned j = i + 1; j < tester.size(); ++j) {
+                    if(tester.at(j) == ']') {
+                        tester.erase(tester.begin() + j);
                     }
                 }
             }
-            // Inserts "test " before the flag if []
-            string str1 = string(tester.begin(), tester.end());
-            if(str1.substr(0,1) == "[") {
-                for(unsigned i = 0; i < str1.length(); ++i) {
-                    if(str1[i] == '-') {
-                        str1.insert(i, "test ");
-                        break;
-                    }
-                }
+        }
+
+        string oldstr3 = string(tester.begin(), tester.end());
+        for(unsigned i = 0; i < oldstr3.length(); ++i) {
+            if(oldstr3[i] == '-') {
+                oldstr3.insert(i, "test ");
+                break;
             }
-            //--------------------------------------------------------
+        }
+       //--------------------------------------------------------------------------------------------
+
+        vector<string> orig;
+
+        for(unsigned i = 0; i < oldstr3.length(); ++i) {
+              if(oldstr3.at(i) == '(') {
+                  oldstr3.insert(i + 1, " ");
+                  i++;
+              }
+        }
+
+          for(unsigned i = 0; i < oldstr3.length(); ++i) {
+                if(oldstr3.at(i) == ')') {
+                    oldstr3.insert(i, " ");
+                    i++;
+                }
+          }
+
+          typedef vector< string > split_vector_type;
+          split_vector_type cnts;
+          split( cnts, oldstr3, is_any_of(" "), token_compress_on );
+          for(unsigned i = 0; i < cnts.size(); ++i) {
+              orig.push_back(cnts.at(i));
+          }
+
+          vector<string> fin;
+          fin.reserve(orig.size() * 5);
+
+          for(int i = 0; i < orig.size(); ++i) {
+              if(orig.at(i) == "(") {
+                  i++;
+                  while(orig.at(i) != ")") {
+                            fin.push_back(orig.at(i));
+                            i++;
+                  }
+              }
+          }
+
+          for(int i = orig.size() - 1; i >= 0; --i) {
+              if(orig.at(i) == "(") {
+                  i--;
+                  fin.push_back(orig.at(i));
+              }
+          }
+
+          for(int i = 0; i < orig.size(); ++i) {
+              if(orig.at(i) == "(") {
+                  break;
+              }
+              fin.push_back(orig.at(i));
+          }
+
+          fin.pop_back();
+
+          for(int i = 0; i < orig.size(); ++i) {
+              if(orig.at(i) == ")") {
+                  i++;
+                  while(i < orig.size()) {
+                    fin.push_back(orig.at(i));
+                    i++;
+                  }
+              }
+          }
+
+          string str1 = boost::join(fin, " ");
+          // cout << str1 << endl;
+
+
+            // Attempt 1: insert contents of parenthesis in front of entire string
+            // Note: This will not work properly. Must find a way to parse string inside parentheses
+            // Possible solution: Base* buildTree(Base* left, Base* right, &string)
+            // Pass in temp string. Return Base* root node and update temp string inside
+
+            // Checks if we need to deal with precedence
+            // if(str.find('(') != std::string::npos) {
+            //     std::size_t leftParenth = str.find('(');
+            //     std::size_t rightParenth = str.rfind(')');
+            //     std::string temp = str.substr(leftParenth + 1, rightParenth - (leftParenth + 1));
+            //     cout << "Original string: " << str << endl;
+            //     cout << "String in Parentheses: " << temp << endl;
+            //     str.erase(leftParenth, (rightParenth + 1) - leftParenth );
+            //     cout << "Original string after removal of parentheses: " << str << endl << endl;
+            //     // str.insert(0, temp + " ");
+            //     // cout << "String after reinsertion: " << str << endl;
+            // }
             if(str1 == "exit") {
                 Exit *bye = new Exit();
                 bye->execute();
@@ -178,18 +246,18 @@ int main() {
                 }
             }
             root->execute();
-        } else {
-            // We need to deal with precedence
-            std::size_t leftParenth = str.find('(');
-            std::size_t rightParenth = str.rfind(')');
-            std::string temp = str.substr(leftParenth + 1, rightParenth - (leftParenth + 1));
-            cout << "Original string: " << str << endl;
-            cout << "String in Parentheses: " << temp << endl;
-            str.erase(leftParenth, (rightParenth + 1) - leftParenth );
-            cout << "Original string after removal of parentheses: " << str << endl << endl;
-            // str.insert(0, temp + " ");
-            // cout << "String after reinsertion: " << str << endl;
-        }
+        // } else {
+        //     // We need to deal with precedence
+        //     std::size_t leftParenth = str.find('(');
+        //     std::size_t rightParenth = str.rfind(')');
+        //     std::string temp = str.substr(leftParenth + 1, rightParenth - (leftParenth + 1));
+        //     cout << "Original string: " << str << endl;
+        //     cout << "String in Parentheses: " << temp << endl;
+        //     str.erase(leftParenth, (rightParenth + 1) - leftParenth );
+        //     cout << "Original string after removal of parentheses: " << str << endl << endl;
+        //     // str.insert(0, temp + " ");
+        //     // cout << "String after reinsertion: " << str << endl;
+        // }
     }
     return 0;
 }
