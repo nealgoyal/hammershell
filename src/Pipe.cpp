eb07629730268ee63e4b2122e406a23f6b17394e
Justin Schopick <jschopick15@gmail.com>
2017-12-07 03:25:57 -0800

    Got pipe working! HAS COMMENTS FOR DEBUGGING

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 
 #include <algorithm>
@@ -16,7 +18,7 @@
 using namespace std;
 using namespace boost;
 
-// class AND : public Connector {
+// class Pipe : public Connector {
 //  public:
 
 // Default Constructor
@@ -24,7 +26,7 @@ Pipe::Pipe() : Connector() {
     data = "";
 }
 
-// Constructor: Sets the left and right-hand sides of the AND connector.
+// Constructor: Sets the left and right-hand sides of the Pipe connector.
 Pipe::Pipe(Base* left, Base* right) {
     lhs = left;
     rhs = right;
@@ -32,45 +34,100 @@ Pipe::Pipe(Base* left, Base* right) {
 }
 
 void Pipe::setpipeVector(std::string str1) {
-
-  std::string str2 = " ";
-  for(unsigned i = 0; i < str1.length(); ++i) {
-      if(str1.at(i) == ';') {
-        str1.insert(i, str2);
-        i++;
-      }
-  }
-
-  typedef vector< string > split_vector_type;
-
-  split_vector_type cnts;
-  split( cnts, str1, is_any_of(" "), token_compress_on );
-  for(unsigned i = 0; i < cnts.size(); ++i) {
-      pipes.push_back(cnts.at(i));
-  }
-
-  for(unsigned i = 0; i < pipes.size(); ++i ) {
-    if(pipes.at(i) == "|") {
-        tempP.push_back(pipes.at(i));
+    std::string str2 = " ";
+    for(unsigned i = 0; i < str1.length(); ++i) {
+        if(str1.at(i) == ';') {
+            str1.insert(i, str2);
+            i++;
+        }
     }
-  }
 
-  pipeV.reserve(tempP.size());
-  for(unsigned index = 0; index < tempP.size(); ++index) {
-      pipeV.push_back((char*)tempP[index].c_str());
-  }
+    typedef vector< string > split_vector_type;
+
+    split_vector_type cnts;
+    split( cnts, str1, is_any_of(" "), token_compress_on );
+    for(unsigned i = 0; i < cnts.size(); ++i) {
+        pipes.push_back(cnts.at(i));
+    }
+
+    for(unsigned i = 0; i < pipes.size(); ++i ) {
+        if(pipes.at(i) == "|") {
+            tempP.push_back(pipes.at(i));
+        }
+    }
+
+    pipeV.reserve(tempP.size());
+    for(unsigned index = 0; index < tempP.size(); ++index) {
+        pipeV.push_back((char*)tempP[index].c_str());
+    }
 }
 
 // Executes right side if left side DOES execute.
 // Returns true if right side executes.
 bool Pipe::execute() {
-    if(lhs->execute()) {
-        if(rhs->execute()) {
-            return true;
-        }
+    int fds[2];
+
+    if(pipe(fds) == -1) {
         return false;
     }
-    return false;
+    cout << "Made pipe" << endl;
+    pid_t pid = fork();
+    if(pid == 0) {
+        cout << "Entered child process" << endl;
+        close(fds[0]);
+        cout << "Closed unused half of lhs" << endl;
+        // Outputs lhs to fds[1] instead of stdout;
+        if(dup2(fds[1], 1) == -1) {
+            cout << "Dup2 failed" << endl;
+            close(fds[1]);
+            exit(1);
+        }
+        if(lhs->execute() == false) {
+            close(fds[1]);
+            exit(1);
+        }
+        cout << "Duplicated new input" << endl;
+        close(fds[1]);
+        cout << "Closed fds[1]" << endl;
+        exit(0);
+    } else if(pid > 0) { // Parent Process
+        cout << "Entered parent process" << endl;
+        close(fds[1]);
+        cout << "Closed unused half of rhs" << endl;
+        int status;
+        waitpid(pid, &status, 0);
+        if(WEXITSTATUS(status) == 1) {
+            return false;
+        }
+        cout << "Child terminated" << endl;
+        // Sends output through as new input and outputs to stdout
+        int userInput = dup(0);
+        cout << "Stores stdin" << endl;
+        if(dup2(fds[0], 0) == -1) {
+            close(fds[0]);
+            dup2(userInput, 0);
+            close(userInput);
+            return false;
+        }
+        cout << "Duplicates input" << endl;
+        if(rhs->execute() == false) {
+            cout << "Couldn't execute rhs" << endl;
+            close(fds[0]);
+            dup2(userInput, 0);
+            close(userInput);
+            return false;
+        }
+        cout << "Executed rhs" << endl;
+        close(fds[0]);
+        dup2(userInput, 0);
+        close(userInput);
+        return true;
+    } else {
+        perror("Fork Failed"); // Failed
+        exit(1);
+    }
+
+    return true;
 }
 
 void Pipe::display() {
